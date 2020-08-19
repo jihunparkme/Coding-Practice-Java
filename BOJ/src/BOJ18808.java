9b333b0455535cf9c61d2217e4ec610ca416aea5
박지훈 <pjh073154@gmail.com>
2020-08-20 01:28:35 +0900

    PS 20200820

--- BOJ/src/BOJ18808.java
+++ BOJ/src/BOJ18808.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BOJ18808 {
@@ -35,25 +36,56 @@ public class BOJ18808 {
 			bfc = nc = C;
 			// 스티커 붙여볼까?
 			findLocate();
-			
-			System.out.println();
 		}
 		
+		int res = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if(notebk[i][j] == 1) 
+					res++;
+			}
+		}
+		
+		System.out.println(res);
 	}
 	
 	// 붙일 수 있는 위치 찾아서 붙이기(가장 위쪽, 가장 왼쪽)
 	public static void findLocate() {
 		// 0, 90, 180, 270도 회전해볼꺼야
 		for (int r = 0; r < 4; r++) {
+			
 			// 붙일 수 있는 위치를 찾다가
-			for (int i = 0; i < nr; i++) {
-				for (int j = 0; j < nc; j++) {
-					// 다른 스티커와 겹치면 pass
+			for (int ntr = 0; ntr < R; ntr++) {
+				for (int ntc = 0; ntc < C; ntc++) {
 					// 노트북을 벗어나면 pass
+					if(ntr + nr > N || ntc + nc > M) continue;
 					
+					// 다른 스티커와 겹치면 pass
+					boolean isPut = true;
+					out:for (int stkr = 0; stkr < nr; stkr++) {
+						for (int stkc = 0; stkc < nc; stkc++) {
+							if(notebk[ntr+stkr][ntc+stkc] == 0 &&
+									sticker[stkr][stkc] == 1) continue;
+							if(notebk[ntr+stkr][ntc+stkc] == 0 &&
+									sticker[stkr][stkc] == 0) continue;
+							if(notebk[ntr+stkr][ntc+stkc] == 1 &&
+									sticker[stkr][stkc] == 0) continue;
+							isPut = false;
+							break out;
+						}
+					}
 					
-					// 붙일 자리가 있다면 붙이기
-					return;
+					// 이 위치에 붙일 수 있다면 붙이기
+					if(isPut) {
+						for (int stkr = 0; stkr < nr; stkr++) {
+							for (int stkc = 0; stkc < nc; stkc++) {
+								if(sticker[stkr][stkc] == 1) 
+									notebk[ntr+stkr][ntc+stkc] = 1;
+							}
+						}
+						// 붙였으면 return
+						return;
+					}
 				}
 			}
 			// 붙일 자리가 없다면 
@@ -70,16 +102,16 @@ public class BOJ18808 {
 		// 기존 모눈종이, 바뀐 모눈종이
 		// 180도 회전은 그대로
 		if(th == 1) {
-			bfr = C;
-			bfc = R;
+			bfr = nr;
+			bfc = nc;
 			nr = R;
 			nc = C;
 			tmpSticker = new int[R][C];
 		}
 		// 90도, 270도는 행열 바뀜 
 		else {
-			bfr = R;
-			bfc = C;
+			bfr = nr;
+			bfc = nc;
 			nr = C;
 			nc = R;
 			tmpSticker = new int[C][R];
@@ -88,15 +120,16 @@ public class BOJ18808 {
 		// 기존 열 -> 행, 기존 행 ->  N(열)-1-행 (열)
 		for (int i = 0; i < bfr; i++) {
 			for (int j = 0; j < bfc; j++) {
-				tmpSticker[j][bfc-1-i] = sticker[i][j];
+				tmpSticker[j][nc-1-i] = sticker[i][j];
 			}
 		}
 		
 		// 스티커 갱신
 		sticker = new int[nr][nc];
 		for (int i = 0; i < nr; i++) {
-			System.arraycopy(tmpSticker, 0, sticker, 0, nc);
+			System.arraycopy(tmpSticker[i], 0, sticker[i], 0, nc);
 		}
+		
 	}
 	
 }
