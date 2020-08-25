66eef6c6f1cbf69ca0b9164bff8d715628af8b43
박지훈 <pjh073154@gmail.com>
2020-08-25 21:54:02 +0900

    PS 20200825

--- BOJ/src/BOJ15686.java
+++ BOJ/src/BOJ15686.java
@@ -37,37 +37,49 @@ public class BOJ15686 {
 		System.out.println(res);
 	}
 
+	/**
+	 * 치킨집을 선택하고, 모든 집들로부터 선택된 치킨집들까지의 가장 가까운 거리의 합 구하기 
+	 * @param start		조합의 시작점 인덱스
+	 * @param cnt		현재까지 뽑은 조합의 개수
+	 */
 	private static void selectChicken(int start, int cnt) {
-		// cnt개의 치킨집을 골랐을 때
+		// M개의 치킨집을 골랐을 때
 		if(cnt == M) {
-			// 모든 집에 대한 정보
+			// 도시의 모든 집에 대한 치킨 거리 정보
 			int all = 0;
-			// 모든 집들부터
+			// 도시의 모든 집들로부터
 			for (int h = 0; h < home.size(); h++) {
 				int dist = 987654321; // 현재 집에 대한 정보
-				// 골라진 치킨집들과 거리를 계산하면서
+				// 선택된 치킨집들까지의 치킨 거리의 최솟값 연산
 				for (int c = 0; c < M; c++) {
 					int tmp = Math.abs(home.get(h).x - selected[c].x);
 					tmp += Math.abs(home.get(h).y - selected[c].y);
 					// 가장 가까운 거리를 찾자
 					dist = Math.min(dist, tmp);
 				}
-				// 현재 집과 가장 가까운 치킨집과의 거리를 누적
 				all += dist;
 			}
-			// 모든 집에 대한 선택된 치킨집들로부터 가장 가까운 거리의 합
+			// 도시에 있는 모든 집들로부터 선택된 치킨집들까지의 치킨 거리의 최솟값
 			res = Math.min(res, all);
 			return;
 		}
-		// 더이상 고를게 없을 경우
+		// 더이상 고를 치킨집이 없을 경우
 		if(start == chicken.size()) return;
-			
+		
 		// 조합 구하기
-		selected[cnt] = chicken.get(start);
-		// start idx에 있는 치킨집을 선택할 경우
-		selectChicken(start + 1, cnt + 1);
-		// start idx에 있는 치킨집을 선택하지 않을 경우
-		selectChicken(start + 1, cnt);
+		for (int i = start; i < chicken.size(); i++) {
+			// i index 치킨집 선택
+			selected[cnt] = chicken.get(i);
+			selectChicken(i + 1, cnt + 1);
+		}
+		
+//		selected[cnt] = chicken.get(start);
+//		// start idx에 있는 치킨집을 선택할 경우
+//		selectChicken(start + 1, cnt + 1);
+//		// start idx에 있는 치킨집을 선택하지 않을 경우
+//		selectChicken(start + 1, cnt);
+		
+		
 	}
 }
 
