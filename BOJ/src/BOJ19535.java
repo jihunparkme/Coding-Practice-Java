ebc9dd41c72ff35c5fc5d99af7364198438dd15c
박지훈 <pjh073154@gmail.com>
2020-08-30 11:13:04 +0900

    PS 20200830

--- BOJ/src/BOJ19535.java
+++ BOJ/src/BOJ19535.java
@@ -6,7 +6,8 @@ import java.util.StringTokenizer;
 
 public class BOJ19535 {
 
-	static int N, res, ans, D, G, leaf;
+	static int N, D, G;
+	static boolean[] checked;
 	static ArrayList<Integer>[] tree;
 	
 	public static void main(String[] args) throws IOException {
@@ -16,6 +17,7 @@ public class BOJ19535 {
 		
 		N = Integer.parseInt(br.readLine());
 		tree = new ArrayList[N+1];
+		checked = new boolean[N+1];
 		for (int i = 1; i <= N; i++) 
 			tree[i] = new ArrayList<>();
 			
@@ -27,79 +29,31 @@ public class BOJ19535 {
 			tree[a].add(b);
 			tree[b].add(a);
 		}
-		
+
+		// ㅈ Tree 찾기
 		for (int i = 1; i <= N; i++) {
-			if(tree[i].size() == 3) G++;
-			else if(tree[i].size() > 3) {
-				ans = 0;
-				countG(tree[i].size(), 0, 0);
-				G += ans;
+			if(tree[i].size() >= 3) {
+				// nC3 = n!/3!(n-3)! = n(n-1)(n-2) / 3 * 2
+				int n = tree[i].size();
+				G += (n * (n-1) * (n-2)) / (3 * 2);
 			}
 		}
-				
-		// 리프 노드 찾기
-		findLeaf(-1, 1);
-		
-		// 리프노드부터 ㄷ Tree 를 찾으러 가보자.
-		findDTree(-1, leaf, 0);
-		go(-1, leaf);
-		
+
+		// ㄷ Tree 찾기
+		for (int parent = 1; parent <= N; parent++) {
+			for (int child = 0; child < tree[parent].size(); child++) {
+				// 이미 확인한 노드일 경우
+				if(checked[tree[parent].get(child)]) continue;
+				// (현재 노드의 간선 - 1) * (연결된 노드의 간선 - 1) 
+				D += (tree[parent].size() - 1) * (tree[tree[parent].get(child)].size() - 1);
+				// check!
+				checked[tree[parent].get(child)] = true;
+			}
+		}
+
 		if(G * 3 < D) System.out.println("D");
 		else if(G * 3 > D) System.out.println("G");
 		else System.out.println("DUDUDUNGA");
-	}
-
-	private static void countG(int R, int cnt, int start) {
-		if (cnt == R) {
-			ans++;
-		}
-		// 현자리에 시작위치 수부터 끝위치 수까지 시도
-		for (int i = start; i < N; i++) {
-			countG(R, cnt + 1, i + 1);
-		}
-		
-	}
-
-	private static void go(int parent, int child) {
-		
-		for (int i = 0; i < tree[child].size(); i++) {
-			// 내 부모 노드면 pass
-			if(tree[child].get(i) == parent) continue;
-			findDTree(-1, tree[child].get(i), 0);
-		}
-		
-	}
-
-	private static void findDTree(int parent, int child, int cnt) {
-		
-		if(cnt == 3) { 
-			D++;
-			return;
-		}
-		
-		for (int i = 0; i < tree[child].size(); i++) {
-			// 내 부모 노드면 pass
-			if(tree[child].get(i) == parent) continue;
-			findDTree(child, tree[child].get(i), cnt + 1);
-		}
-		
-	}
-
-	private static void findLeaf(int parent, int child) {
-		// leafNode를 찾았으면
-		if(leaf != 0) return;
-		// leafNode 발견!
-		if(tree[child].size() == 1) {
-			leaf = child;
-			return;
-		}
-		
-		for (int i = 0; i < tree[child].size(); i++) {
-			// 내 부모 노드면 pass
-			if(tree[child].get(i) == parent) continue;
-			findLeaf(child, tree[child].get(i));
-		}
-		
 	}
 	
 }
