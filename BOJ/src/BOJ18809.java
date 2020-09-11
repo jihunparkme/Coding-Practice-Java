312d21fda810a5fe39a75578956ef73138c2829c
박지훈 <pjh073154@gmail.com>
2020-09-11 23:25:32 +0900

    PS 20200911

--- BOJ/src/BOJ18809.java
+++ BOJ/src/BOJ18809.java
@@ -3,7 +3,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -24,12 +23,6 @@ public class BOJ18809 {
 			this.y = y;
 			this.color = color;
 		}
-
-		@Override
-		public String toString() {
-			return "State [x=" + x + ", y=" + y + ", color=" + color + "]";
-		}
-		
 	}
 	
 	public static void main(String[] args) throws IOException {
@@ -41,10 +34,10 @@ public class BOJ18809 {
 		M = Integer.parseInt(st.nextToken());	// 열
 		G = Integer.parseInt(st.nextToken());	// 초록색 배양액 개수
 		R = Integer.parseInt(st.nextToken());	// 빨간색 배양액 개수
-		allDrop = G + R;
+		allDrop = G + R;	// 전체 배양액 개수
 		map = new int[N][M];
-		select = new State[allDrop];
-		area = new ArrayList<>();
+		select = new State[allDrop];	// 배양액이 뿌려질 좌표 저장
+		area = new ArrayList<>();	// 배양액을 뿌릴 수 있는 좌표들 저장
 		// 정원 정보 입력
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -54,64 +47,99 @@ public class BOJ18809 {
 				if(map[i][j] == 2) area.add(new Point(i, j));
 			}
 		}
-		
-		go(0, 0, 0);
+	
+		go(0, 0, 0, 0);
 		System.out.println(res);
 	}
 
-	private static void go(int cnt, int cntG, int cntR) {
-		if(cntG + cntR > area.size() - cnt) return;
-		if(cnt == area.size()) return;
+	/**
+	 * 배양액 뿌리기
+	 * @param idx	배양액을 뿌릴 수 있는 좌표 index
+	 * @param cnt	현재까지 뿌린 배양액의 갯수
+	 * @param cntG	현재까지 뿌린 초록 배양액의 갯수
+	 * @param cntR  현재까지 뿌린 빨강 배양액의 갯수
+	 */
+	private static void go(int idx, int cnt, int cntG, int cntR) {
 		// 모든 배양액을 사용했을 경우
 		if(cnt == allDrop) {
 			// 배양액이 퍼져나간다.
 			// 피울 수 있는 꽃의 최대 개수
 			res = Math.max(res, spread());
-//			System.out.println(Arrays.toString(select));
 			return;
 		}
+		if(idx == area.size()) 
+			return;
 		
-		Point now = area.get(cnt);
+		Point now = area.get(idx);
 		
 		// 여기에 초록색 배양액 뿌리기
 		if(cntG < G) {
 			select[cnt] = new State(now.x, now.y, 1);
-			go(cnt + 1, cntG + 1, cntR);
+			go(idx + 1, cnt + 1, cntG + 1, cntR);
 		}
 		
 		// 여기에 빨간색 배양액 뿌리기
 		if(cntR < R) {
 			select[cnt] = new State(now.x, now.y, 2);
-			go(cnt + 1, cntG, cntR + 1);
+			go(idx + 1, cnt + 1, cntG, cntR + 1);
 		}
 		
 		// 여기에 아무것도 뿌리지 않기
-		go(cnt + 1, cntG, cntR);
+		go(idx + 1, cnt, cntG, cntR);
 	}
 
+	// 배양액 퍼뜨리기
 	private static int spread() {
-		
+				
+		// 배양액이 퍼지는데 걸린 시간 저장
 		int[][] visitedGrn = new int[N][M];
 		int[][] visitedRed = new int[N][M];
-		int cnt = 0, time = 1;
-		// 큐에 배양액에 뿌려져있는 좌표를 넣고
-		Queue<State> q = new LinkedList<>();
+		
+		int cnt = 0;
+		// 먼저 큐에 배양액이 뿌려져있는 좌표를 넣고
+		Queue<State> green = new LinkedList<>();
+		Queue<State> red = new LinkedList<>();
 		for (int i = 0; i < allDrop; i++) {
 			State tmp = select[i];
-			q.add(new State(tmp.x, tmp.y, tmp.color));
 			// 배양액에 뿌려진 곳은 1로 초기화
-			if(tmp.color == 1) visitedGrn[tmp.x][tmp.y] = time;
-			else visitedRed[tmp.x][tmp.y] = time;
+			if(select[i].color == 1) {
+				green.add(new State(tmp.x, tmp.y, tmp.color));
+				visitedGrn[tmp.x][tmp.y] = 1;
+			}
+			else {
+				red.add(new State(tmp.x, tmp.y, tmp.color));
+				visitedRed[tmp.x][tmp.y] = 1;
+			}
 		}
 		
 		// 배양액이 모두 퍼질 때까지
-		while(!q.isEmpty()) {
+		while(!green.isEmpty() || !red.isEmpty()) {
 			
-			int size = q.size();
-			time++;
-			
+			// 초록색 배양액부터 퍼진다.			
+			int size = green.size();
 			for (int s = 0; s < size; s++) {
-				State now = q.poll();
+				State now = green.poll();
+				// 꽃이 핀 자리라면 pass
+				if(visitedGrn[now.x][now.y] == -100) continue;
+ 				// 4방 탐색
+				for (int d = 0; d < 4; d++) {
+					int xx = now.x + dx[d];
+					int yy = now.y + dy[d];
+					// 범위를 벗어날 경우, 호수가 있을 경우 pass
+					if(xx < 0 || yy < 0 || xx >= N || yy >= M || map[xx][yy] == 0) continue;
+					// 이미 같은 배양액이 뿌려져 있다면 pass
+					if(visitedGrn[xx][yy] != 0) continue;
+
+					// 초록색 배양액이 뿌려진 적이 없다면 퍼진다
+					visitedGrn[xx][yy] = visitedGrn[now.x][now.y] + 1;
+					green.add(new State(xx, yy, now.color));
+				}
+			}
+			
+			// 다음으로 빨간색 배양액부터 퍼진다.			
+			size = red.size();
+			for (int s = 0; s < size; s++) {
+				State now = red.poll();
 				
 				// 4방 탐색
 				for (int d = 0; d < 4; d++) {
@@ -119,24 +147,22 @@ public class BOJ18809 {
 					int yy = now.y + dy[d];
 					// 범위를 벗어날 경우, 호수가 있을 경우 pass
 					if(xx < 0 || yy < 0 || xx >= N || yy >= M || map[xx][yy] == 0) continue;
-					// 이미 배양액이 도달한 적이 있다면 pass
-					if((now.color == 1 && visitedGrn[xx][yy] >= 1) ||
-							(now.color == 2 && visitedRed[xx][yy] >= 1)) continue;
+					// 이미 같은 배양액이 뿌려져 있다면 pass
+					if(visitedRed[xx][yy] != 0) continue;
 					
-					// 초록색 배양액과 빨간색 배양액이 동일한 시간에 도달한 땅에서는 두 배양액이 합쳐져서 꽃이 피어난다.
-					// 꽃이 피어난 땅에서는 배양액이 사라지기 때문에 더 이상 인접한 땅으로 배양액을 퍼트리지 않는다.
-					if((now.color == 1 && time == visitedRed[xx][yy]) ||
-							(now.color == 2 && time == visitedGrn[xx][yy])) {
+					visitedRed[xx][yy] = visitedRed[now.x][now.y] + 1;
+					
+					// 초록색 배양액이 동일한 시간대에 뿌려졌다면 꽃이 핀다.
+					if(visitedGrn[xx][yy] == visitedRed[xx][yy]) {
 						cnt++;
-						visitedGrn[xx][yy] = 100;
-						visitedRed[xx][yy] = 100;
+						// 꽃이 핀 자리는 -100 저장 
+						visitedGrn[xx][yy] = -100;
+						visitedRed[xx][yy] = -100;
 						continue;
 					}
-
-					// 해당 색 배양액이 도달한 적이 없다면 퍼진다
-					// queue에 ass
-					visitedGrn[xx][yy] = time;
-					q.add(new State(xx, yy, now.color));
+					
+					// 그 외의 경우는 배양액이 퍼진다
+					red.add(new State(xx, yy, now.color));
 				}
 			}
 		}
@@ -144,6 +170,3 @@ public class BOJ18809 {
 		return cnt;
 	}
 }
-
-
-// 0은 호수, 1은 배양액을 뿌릴 수 없는 땅, 2는 배양액을 뿌릴 수 있는 땅
