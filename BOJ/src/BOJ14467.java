2286fc7a1354501dd17ef01eaa18f41eae85f4a7
박지훈 <pjh073154@gmail.com>
2020-10-03 19:03:52 +0900

    PS 20201003

---
+++ BOJ/src/BOJ14467.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ14467 {
+
+	static int N;
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		N = Integer.parseInt(br.readLine());
+
+	}
+
+}
