dcbe1e5b7ccbdefdd74d2673540c341d61958be3
박지훈 <pjh073154@gmail.com>
2020-10-03 19:56:31 +0900

    PS 20201003

--- BOJ/src/BOJ14467.java
+++ BOJ/src/BOJ14467.java
@@ -1,11 +1,12 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BOJ14467 {
 
-	static int N;
+	static int N, observe[];
 	
 	public static void main(String[] args) throws IOException {
 		
@@ -13,7 +14,24 @@ public class BOJ14467 {
 		StringTokenizer st;
 		
 		N = Integer.parseInt(br.readLine());
-
+		observe = new int[N];
+		Arrays.fill(observe, -1);
+		int cnt = 0;
+		for (int i = 0; i < N; i++) {
+		
+			st = new StringTokenizer(br.readLine());
+			
+			int c = Integer.parseInt(st.nextToken()) - 1;
+			int p = Integer.parseInt(st.nextToken());
+			
+			if(observe[c] == -1) observe[c] = p;
+			else if(observe[c] != p) {
+				observe[c] = p;
+				cnt++;
+			}
+		}
+		
+		System.out.println(cnt);
 	}
 
 }
