eff4c383a33763f7de371ccfcebbfda63aaa1f0e
박지훈 <pjh073154@gmail.com>
2020-10-07 00:58:26 +0900

    PS 20201007

--- BOJ/src/Main.java
+++ BOJ/src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer
+		StringTokenizer st;
 	}
 	
 }
