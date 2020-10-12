5a7b4805127fdcd997af3a85813bfee7f9ea412c
박지훈 <pjh073154@gmail.com>
2020-10-13 00:00:56 +0900

    PS 20201012

--- BOJ/src/BOJ15684.java
+++ BOJ/src/BOJ15684.java
@@ -14,19 +14,17 @@ public class BOJ15684 {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		N = Integer.parseInt(st.nextToken()); // 세로선 개수
-		M = Integer.parseInt(st.nextToken()); // 가로선 개수
-		H = Integer.parseInt(st.nextToken()); // 놓을 수 있는 선
+		M = Integer.parseInt(st.nextToken()); // 기존에 놓인 가로선의 개수
+		H = Integer.parseInt(st.nextToken()); // 가로 점선 개수
 		ladder = new boolean[H][N];
 
 		// 가로선 정보
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-			// b번 세로선과 b+1번 세로선을 a번 점선 위치에서 연결
 			int a = Integer.parseInt(st.nextToken()) - 1;
 			int b = Integer.parseInt(st.nextToken()) - 1;
-
+			// a번과 b번 사이에 가로선
 			ladder[a][b] = true;
-			ladder[a][b + 1] = true;
 		}
 
 		System.out.println(go());
@@ -35,13 +33,13 @@ public class BOJ15684 {
 	private static int go() {
 		
 		int i = 0;
-		// 놓을 수 있는 가로선 개수
+		// 추가로 놓을 수 있는 가로선 개수는 최대 3개
 		for (i = 0; i <= 3; i++) {
 
-			// 0일 경우는 바로 확인 후 continue;
+			// 0일 경우는 바로 결과 확인
 			if(i == 0) {
 				// 조작 성공?!
-				if(start()) break;
+				if(check()) break;
 				else continue;
 			}
 			
@@ -57,59 +55,51 @@ public class BOJ15684 {
 		
 	}
 
-	private static void findPosition(int start, int n, int cnt) {
+	private static void findPosition(int idx, int n, int cnt) {
 
 		if(isFinish) return;
 		// 놓을 위치를 정했다면
 		if (cnt == n) {
 			// 사타리를 타고 내려가보자.
-			if(start()) isFinish = true;
+			if(check()) isFinish = true;
 			return;
 		}
 
-		for (int i = start; i < N * H; i++) {
+		for (int i = idx; i < H * N; i++) {
 			int x = i / N;
 			int y = i % N;
 			// 가로선을 놓을 수 있는 범위를 넘어가면
 			if(y >= N - 1) continue;
 			// 가로선을 놓을 수 없는 자리면 pass
-			if(ladder[x][y] || ladder[x][y + 1] || (y - 1 >= 0 && ladder[x][y - 1])) continue;
+			// 좌 or 우측 방향에 가로선이 이미 있을 경우
+			if((y - 1 >= 0 && ladder[x][y - 1]) || ladder[x][y]) continue;
 			
 			// 여기에 가로 선을 놓아보자.
-			setLine(x, y, true);
+			ladder[x][y] = true;
 			findPosition(i + 1, n, cnt + 1);
 
 			// 놓았을 경우 되돌려야지.
-			setLine(x, y, false);
-			
-			// 여기에 가로 선을 안 놓을래
-			findPosition(i + 1, n, cnt);
+			ladder[x][y] = false;
 		}
 	}
 
-	private static void setLine(int x, int y, boolean state) {
+	private static boolean check() {
 		
-		ladder[x][y] = state;
-		ladder[x][y + 1] = state;
-		
-	}
-
-	private static boolean start() {
-		
-		// i번부터 내려가보자.
-		for (int c = 0; c < N - 1; c++) {
+		// c번부터 확인
+		for (int c = 0; c < N; c++) {
 			
 			int now = c;
+			
 			for (int r = 0; r < H; r++) {
-				// 가로선이 없으면 내려가기
-				if(!ladder[r][now]) continue;
+				// 양쪽에 가로선이 없으면 그냥 내려가기
+				if(!(now - 1 >= 0 && ladder[r][now - 1]) && !ladder[r][now]) continue;
 				// 가로선이 있으면
 				else {
-					// 어느 방향(좌/우)에 연결되어있는지 확인
+					// 어느 방향(좌/우)에 가로선이 있는지 확인
 					// 왼쪽으로 연결?
 					if(now - 1 >= 0 && ladder[r][now - 1]) now -= 1;
 					// 오른쪽으로 연결?
-					else if(ladder[r][now + 1]) now += 1;
+					else if(ladder[r][now]) now += 1;
 				}
 			}
 			
