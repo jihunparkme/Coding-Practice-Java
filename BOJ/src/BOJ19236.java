080515b215e9070c4b361db0f112e5fb6b51c623
박지훈 <pjh073154@gmail.com>
2020-10-15 00:54:58 +0900

    PS 20201015

--- BOJ/src/BOJ19236.java
+++ BOJ/src/BOJ19236.java
@@ -1,4 +1,3 @@
-import java.awt.Point;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -26,7 +25,7 @@ public class BOJ19236 {
 		StringTokenizer st;
 
 		int[][] map = new int[N][N];
-		Fish[] fishPos = new Fish[17];
+		Fish[] fishes = new Fish[17];
 		// 공간 정보 입력
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -36,87 +35,84 @@ public class BOJ19236 {
 				int b = Integer.parseInt(st.nextToken()) - 1; // 방향
 				
 				map[i][j] = a;
-				fishPos[a] = new Fish(i, j, b);
+				fishes[a] = new Fish(i, j, b);
 			}
 		}
 		
 		// 먼저 상어가 (0,0)공간에 들어간다.
 		// (0,0)에 있는 물고기를 먹고
-		res = map[0][0].num;
-		// 먹힌 고기는 null
-		fishPos[map[0][0].num] = null;
+		res = map[0][0];
 		// (0,0)에 들어간다.
 		// 상어의 방향은 (0, 0)에 있던 물고기의 방향
-		Shark shark = new Shark(0, 0, map[0][0].dir);
+		Fish shark = new Fish(0, 0, fishes[map[0][0]].dir);
+		// 먹힌 고기는 null
+		fishes[map[0][0]] = null;
+
 		// 상어를 공간에 위치 (상어는 -1 번)
-		map[0][0] = new Fish(-1, -1);
+		map[0][0] = -1;
 		
-		process(shark, map, fishPos, res);
+		process(map, shark, fishes, res);
 		
 		System.out.println(res);
 		
 	}
 
-	private static void process(Shark shark, Fish[][] map, Point[] fishPos, int sum) {
-		
-		Fish[][] newMap = new Fish[N][N];
+	private static void process(int[][] map, Fish shark, Fish[] fishes, int sum) {
+
+		System.out.println("초기 이동 후");
 		for (int i = 0; i < N; i++) {
-			newMap[i] = map[i].clone();
+			for (int j = 0; j < N; j++) {
+				System.out.print(map[i][j] + " ");
+			}
+			System.out.println();
 		}
+		System.out.println("\n\n\n");
+		
+		// 데이터 복사
+		int[][] origMap = new int[N][N];
+		for (int i = 0; i < N; i++) 
+			System.arraycopy(map[i], 0, origMap[i], 0, N);
+		
+		Fish origShark = new Fish(shark.x, shark.y, shark.dir);
+		Fish[] origFishes = new Fish[17];
+		System.arraycopy(fishes, 0, origFishes, 0, fishes.length); 
 		
 		// 먼저 물고기가 이동
-		for (int i = 1; i <= 16; i++) {
+		for (int f = 1; f <= 16; f++) {
 			
 			// i번 물고기의 좌표
-			Point now = fishPos[i];
+			Fish now = fishes[f];
 			// 먹힌 물고기면
 			if(now == null) continue;
-			
-			Fish fish = map[now.x][now.y];
-			
-			// 물고기는 한 칸을 이동
-			int xx = now.x + dx[fish.dir];
-			int yy = now.y + dy[fish.dir];
-			
-			boolean isMove = true;
-			// 상어가 있거나, 공간을 넘어가면
-			if(xx < 0 || xx >= N || yy < 0 || yy >= N || 
-					(map[xx][yy] != null && map[xx][yy].num == -1)) {
-				// 이동할 수 있는 칸을 향할 때까지 방향을 45도 반시계 회전
+						
+			// 이동할 수 있는 칸을 향할 때까지 방향을 45도 반시계 회전
+			int origDir = now.dir;
+			do {
+				// 물고기는 한 칸을 이동
+				int xx = now.x + dx[now.dir];
+				int yy = now.y + dy[now.dir];
 				
-				int initPos = fish.dir; 
-				fish.dir = (fish.dir + 1) % 8; 
-				isMove = false;
-				
-				while(fish.dir != initPos) {
-					
-					xx = now.x + dx[fish.dir];
-					yy = now.y + dy[fish.dir];
-					// 이동할 수 없다면 45도 반시계 회전
-					if(xx < 0 || xx >= N || yy < 0 || yy >= N || 
-							(map[xx][yy] != null && map[xx][yy].num == -1)) {
-						fish.dir = (fish.dir + 1) % 8; 
-						continue;
-					}
-					// 이동할 수 있다면
-					isMove = true;
-					break;
+				// 상어가 있거나, 공간을 넘어가면 다음 회전
+				if(xx < 0 || xx >= N || yy < 0 || yy >= N || map[xx][yy] == -1) {
+					now.dir = (now.dir + 1) % 8;
+					continue;
 				}
-			}
+				
+				// 빈 칸, 다른 물고기가 있는 칸이라면 이동하고 위치를 바꿔주자
+				// map과 물고기 위치 갱신
+				Fish ftmp = fishes[map[xx][yy]];
+				fishes[map[xx][yy]] = fishes[f];
+				fishes[f] = ftmp;
+				
+				int ntmp = map[xx][yy];
+				map[xx][yy] = f;
+				map[now.x][now.y] = ntmp;
+				
+				break;
+				
+			} while (now.dir != origDir);
+			
 			// 이동할 수 있는 칸이 없으면 이동을 하지 않는다
-			if(!isMove) continue;
-			
-			// 빈 칸, 다른 물고기가 있는 칸이라면 이동하고 위치를 바꿔주자
-			// map과 물고기 위치 갱신
-			Point ptmp = fishPos[i]; 
-			fishPos[i] = new Point(xx, yy);
-			// 빈 칸이 아니라면 
-			if(map[xx][yy] != null)
-				fishPos[map[xx][yy].num] = ptmp;
-			
-			Fish ftmp = map[xx][yy];
-			newMap[xx][yy] = fish;			
-			newMap[now.x][now.y] = ftmp;
 		}
 		
 		System.out.println("물고기 이동 후");
@@ -126,35 +122,32 @@ public class BOJ19236 {
 			}
 			System.out.println();
 		}
-		System.out.println("\n\n\n");
+		System.out.println("\n\n");
 		
 		// 상어가 이동
-		// 초기 상어 위치
-		int xx = shark.x;
-		int yy = shark.y;
-		boolean isMove = false;
-		// 이동할 수 있을 때까지 다 이동해보자.
-		while(true) {
-			xx += dx[shark.dir];
-			yy += dy[shark.dir];
+		// 이동할 수 있을 때까지 다 이동해보자(최대 3칸)
+		for (int d = 1; d <= 3; d++) {
+			int xx = shark.x + dx[shark.dir] * d;
+			int yy = shark.y + dy[shark.dir] * d;
 			
 			// 범위 넘어가면
 			if(xx < 0 || xx >= N || yy < 0 || yy >= N) break;
 			// 물고기가 없는 칸일 경우
-			if(newMap[xx][yy] == null) continue;
+			if(map[xx][yy] == 0) continue;
 			
 			// 상어가 물고기가 있는 칸으로 이동했다면
-			// 그 칸에 있는 물고기를 먹고, 그 물고기의 방향을 가지게 된다. 
-			Fish fish = newMap[xx][yy];
+			// 그 칸에 있는 물고기를 먹고, 그 물고기의 방향을 가지게 된다.
+			int num = map[xx][yy];
+			Fish fish = fishes[map[xx][yy]];
 			
 			// 물고기 정보에서 삭제
-			fishPos[fish.num] = null;
+			fishes[map[xx][yy]] = null;
+			// 상어 정보 갱신
+			shark = new Fish(xx, yy, fish.dir);
 			// map 갱신
-			newMap[xx][yy] = new Fish(-1, -1);
-			// 원래 상어가 있던 위치는 null
-			newMap[shark.x][shark.y] = null;
-
-			isMove = true;
+			map[xx][yy] = -1;
+			// 원래 상어가 있던 위치는 비우자
+			map[fish.x][fish.y] = 0;
 			
 			System.out.println("상어 이동 후");
 			for (int i = 0; i < N; i++) {
@@ -165,22 +158,24 @@ public class BOJ19236 {
 			}
 			System.out.println("\n\n\n");
 			
-			process(new Shark(xx, yy, fish.dir), newMap, fishPos, sum + fish.num);
+			process(map, shark, fishes, sum + num);
 			
 			// 백트래킹
-
+	
+			map[fish.x][fish.y] = -1;
+			map[xx][yy] = num;
+			shark = new Fish(origShark.x, origShark.y, origShark.dir);
+			fishes[map[xx][yy]] = fish;
+			
 			// 물고기 정보 복구
-			fishPos[fish.num] = new Point(xx, yy);
+//			for (int i = 0; i < N; i++) 
+//				System.arraycopy(origMap[i], 0, map[i], 0, N);
+//			System.arraycopy(origFishes, 0, fishes, 0, origFishes.length); 
 		}
 		
 		// 상어가 이동할 수 있는 칸이 없으면 공간에서 벗어나 집으로 간다.
-		if(!isMove) {
-			// 결과 갱신
-			res = Math.max(res, sum);
-			
-			System.out.println("이동 불가");
-			return;
-		}
+		// 결과 갱신
+		res = Math.max(res, sum);
 		
 	}
 
@@ -213,4 +208,8 @@ public class BOJ19236 {
 	이동하는 중에 지나가는 칸에 있는 물고기는 먹지 않는다.
 	물고기가 없는 칸으로는 이동할 수 없다.
 	상어가 이동할 수 있는 칸이 없으면 공간에서 벗어나 집으로 간다.
+	
+배열을 복사하는 이유는 매개변수로 들어오는 배열, 객체는 포인트를 가리키고 있어서 힙에 하나만 존재
+그래서 복사를 해서 원본 데이터를 보존
+
 */
