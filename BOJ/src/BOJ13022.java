2f74cd01c940c74303f08dd1d9ff58e6ee8ea3b7
박지훈 <pjh073154@gmail.com>
2020-10-25 01:25:47 +0900

    PS 20201025

--- BOJ/src/BOJ13022.java
+++ BOJ/src/BOJ13022.java
@@ -3,50 +3,69 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class BOJ13022 {
-	
+
+	static char[] wolf = { 'w', 'o', 'l', 'f' };
+
 	public static void main(String[] args) throws IOException {
-		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		String input = br.readLine();
-		
+
 		System.out.println(process(input));
 	}
 
 	private static int process(String str) {
+
+		char[] arr = new char[4];
+		int[] cnt = new int[26];
+		// 첫 문자는 먼저 넣어두자.
+		char prev = str.charAt(0);
+		arr[0] = str.charAt(0);
+		cnt[str.charAt(0) - 'a']++;
+		int idx = 1;
 		
-		int[] cnt = new int[4];
-		for (int i = 0; i < str.length(); i++) {
-			switch(str.charAt(i)) {
-			case 'w':
-				cnt[0]++;
-				break;
-			case 'o':
-				cnt[1]++;
-				if(cnt[0] != cnt[1]) return 0;
-				break;
-			case 'l':
-				cnt[2]++;
-				if(cnt[1] != cnt[2]) return 0;
-				break;
-			case 'f':
-				cnt[3]++;
-				if(cnt[2] != cnt[3]) return 0;
-				break;
+		for (int i = 1; i < str.length(); i++) {
+			char now = str.charAt(i);
+			// 이전 문자와 같다면 pass
+			if(now == prev) {
+				cnt[now - 'a']++;
+				continue;
 			}
+			
+			// 다르다면
+			// 'w'로 다시 시작할 경우.
+			if(idx == 4) {
+				// 순서 및 반복횟수 확인
+				if(!check(arr, cnt)) return 0;
+				
+				// cnt 배열과 index 초기화
+				cnt = new int[26];
+				idx = 0;
+			}
+			
+			arr[idx++] = now;
+			cnt[now - 'a']++;
+			prev = now;
 		}
+
+		// 길이가 4인 경우도 확인 필요
+		if(!check(arr, cnt)) return 0;
 		
 		return 1;
 	}
-	
+
+	private static boolean check(char[] arr, int[] cnt) {
+		
+		int flag = cnt[arr[0] - 'a'];
+		for (int ck = 0; ck < 4; ck++) {
+			// 순서 확인
+			if(arr[ck] != wolf[ck]) return false;
+			// 반복횟수 확인
+			if(cnt[arr[ck] - 'a'] != flag) return false;
+		}
+		
+		return true;
+	}
+
 }
-
-/*
-임의의 양의 정수 n에 대해서, 'w'가 n번 나오고, 
-그 다음에 'o'가 n번, 그 다음에 'l'이 n번, 
-그 다음에 'f'가 n번 나온 단어는 올바른 단어이다.
-
-올바른 단어 두 개를 이은 단어도 올바른 단어이다.
-
-1번과 2번 조건으로 만들 수 있는 단어만 올바른 단어이다.
-*/
