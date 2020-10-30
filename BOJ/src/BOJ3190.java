bb1e3e552a888ad64bff576672b5b88b92a52582
박지훈 <pjh073154@gmail.com>
2020-10-30 21:48:01 +0900

    PS 20201030

--- BOJ/src/BOJ3190.java
+++ BOJ/src/BOJ3190.java
@@ -1,4 +1,3 @@
-import java.awt.Point;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -14,6 +13,7 @@ public class BOJ3190 {
 	static int[] dx = { 0, 1, 0, -1 }, dy = { 1, 0, -1, 0 }; // 동,남,서,북
 	static Map<Integer, Character> oper;
 	static Body head;
+
 	static class Body {
 		int x, y, dir;
 
@@ -25,16 +25,6 @@ public class BOJ3190 {
 		}
 
 		@Override
-		public int hashCode() {
-			final int prime = 31;
-			int result = 1;
-			result = prime * result + dir;
-			result = prime * result + x;
-			result = prime * result + y;
-			return result;
-		}
-
-		@Override
 		public boolean equals(Object obj) {
 			if (this == obj)
 				return true;
@@ -43,15 +33,12 @@ public class BOJ3190 {
 			if (getClass() != obj.getClass())
 				return false;
 			Body other = (Body) obj;
-			if (dir != other.dir)
-				return false;
 			if (x != other.x)
 				return false;
 			if (y != other.y)
 				return false;
 			return true;
-		}
-		
+		}		
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -83,42 +70,47 @@ public class BOJ3190 {
 
 		int time = 0;
 		Queue<Body> body = new LinkedList<>();
-		// 시작 위치는  (0,0)
+		// 시작 위치는 (0,0)
 		body.add(new Body(0, 0, 0));
 		head = new Body(0, 0, 0);
-		
+
 		while (true) {
 
-			++time;
-			Body nextHead = new Body(0, 0, 0);
-			
+			time++;
+			Body nextHead = new Body(head.x, head.y, head.dir);
+
+			nextHead.x += dx[nextHead.dir];
+			nextHead.y += dy[nextHead.dir];
+
+			// 머리가 벽 또는 자기자신의 몸과 부딪히면 게임이 끝난다
+			if (body.contains(nextHead) 
+					|| nextHead.x < 0 || nextHead.x >= N || nextHead.y < 0 || nextHead.y >= N)
+				return time;
+
+			// 이동한 칸에 사과가 있다면, 그 칸에 있던 사과는 없어지고 뱀 머리로
+			if (map[nextHead.x][nextHead.y] == 1) {
+				map[nextHead.x][nextHead.y] = 0;
+			}
+			// 이동한 칸에 사과가 없다면, 몸 길이를 줄여서 꼬리가 위치한 칸을 비워준다.
+			else {
+				body.poll();
+			}
+
+			// 방향 전환
 			if (oper.containsKey(time)) {
 				char d = oper.get(time);
 				// 왼쪽(C가 'L') 또는 오른쪽(C가 'D')
-				if(d == 'D') {
-					nextHead.dir = (head.dir + 1) % 4; 
+				if (d == 'D') {
+					nextHead.dir = (nextHead.dir + 1) % 4;
 				} else {
-					nextHead.dir = (head.dir + 3) % 4; 
+					nextHead.dir = (nextHead.dir + 3) % 4;
 				}
 			}
 			
-			nextHead.x = head.x + dx[nextHead.dir];
-			nextHead.y = head.y + dy[nextHead.dir];
-			
-			// 머리가 벽 또는 자기자신의 몸과 부딪히면 게임이 끝난다
-			if(body.contains(nextHead) ||
-					nextHead.x < 0 || nextHead.x >= N || nextHead.y < 0 || nextHead.y >= N) return time;
-			
-			return time;
+			// 머리를 몸에 추가
+			body.add(nextHead);
+			// 머리 정보 갱신
+			head = new Body(nextHead.x, nextHead.y, nextHead.dir);
 		}
 	}
-
 }
-
-/*
- * 
- * 
- * 매 초 이동하는데 규칙 몸길이를 늘려 머리를 다음칸에 위치 만약 이동한 칸에 사과가 있다면, 그 칸에 있던 사과가 없어지고 꼬리는 움직이지
- * 않는다. 만약 이동한 칸에 사과가 없다면, 몸길이를 줄여서 꼬리가 위치한 칸을 비워준다. 즉, 몸길이는 변하지 않는다.
- * 
- */
