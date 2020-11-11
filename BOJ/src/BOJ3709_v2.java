4a3ac7232bab5c6225e462884ea7bb8872f1fe9c
박지훈 <pjh073154@gmail.com>
2020-11-11 15:52:18 +0900

    PS 20201111

--- BOJ/src/BOJ3709_v2.java
+++ BOJ/src/BOJ3709_v2.java
@@ -3,10 +3,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class BOJ3709 {
+public class BOJ3709_v2 {
 
-	static int N, R, resR, resC;
-	static boolean map[][], isFin;
+	static int N, R, resR, resC, map[][];
 	static int[] dr = {-1, 0, 1, 0}, dc = {0, 1, 0, -1};
 
 	public static void main(String[] args) throws IOException {
@@ -16,7 +15,6 @@ public class BOJ3709 {
 
 		int T = Integer.parseInt(br.readLine());
 		for (int tc = 1; tc <= T; tc++) {
-			isFin = false;
 			resR = 0;
 			resC = 0;
 			
@@ -24,7 +22,7 @@ public class BOJ3709 {
 		
 			N = Integer.parseInt(st.nextToken());
 			R = Integer.parseInt(st.nextToken());
-			map = new boolean[N + 2][N + 2];
+			map = new int[N + 2][N + 2];
 
 			int a = 0, b = 0;
 			// 거울 배치
@@ -34,7 +32,7 @@ public class BOJ3709 {
 				a = Integer.parseInt(st.nextToken());
 				b = Integer.parseInt(st.nextToken());
 				
-				map[a][b] = true; 
+				map[a][b] = 4; 
 			}
 			// 레이저 배치
 			st = new StringTokenizer(br.readLine());
@@ -47,34 +45,40 @@ public class BOJ3709 {
 			else if(b == 0) dir = 1;
 			else if(b == N + 1) dir = 3; 
 			
-			process(a, b, dir, 0);
+			process(a, b, dir);
 			
 			System.out.println(resR + " " + resC);
 		}
 
 	}
 
-	private static void process(int r, int c, int d, int cnt) {
+	private static boolean process(int r, int c, int d) {
 		
-		if(isFin) return;
-		
-		if(cnt > N * N ) return;
-		
+		// 다음 좌표
 		int rr = r + dr[d];
 		int cc = c + dc[d];
-		// 레이저가 보드를 벗어날 경우
+		// 레이저가 보드를 벗어날 경우 종료
 		if(rr < 1 || rr > N || cc < 1 || cc > N) {
 			resR = rr;
 			resC =cc;
-			isFin = true;
-			
-			return;
+			return true;
 		}
 		
 		// 거울이 없을 경우
-		if(!map[rr][cc]) process(rr, cc, d, cnt);
-		// 거울이 있을 경우 오른쪽으로 각도를 변경
-		else process(rr, cc, (d + 1) % 4, cnt + 1);
+		if(map[rr][cc] == 0) {
+			if(process(rr, cc, d)) return true;
+		}
+		// 거울이 있을 경우 레이저는 우측으로 반사
+		else {
+			int nextDir = (d + 1) % 4;
+			// 이전에 같은 방향으로 온 적이 있다면 빔이 보드 밖을 벗어나지 않는 상황
+			if(map[rr][cc] == nextDir) return true;
+			map[rr][cc] = nextDir;
+			
+			if(process(rr, cc, (d + 1) % 4)) return true;;
+		}
+		
+		return false;
 	}
 
 }
