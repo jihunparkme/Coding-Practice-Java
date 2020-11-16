00f6e0a0a09e103a084947ba93dcf55aeb8ec301
박지훈 <pjh073154@gmail.com>
2020-11-16 23:46:42 +0900

    PS 20201116

--- BOJ/src/BOJ8972.java
+++ BOJ/src/BOJ8972.java
@@ -9,7 +9,7 @@ public class BOJ8972 {
 
 	static int R, C, IR, IC;
 	static int[] dr = { 0, 1, 1, 1, 0, 0, 0, -1, -1, -1 }, dc = { 0, -1, 0, 1, -1, 0, 1, -1, 0, 1 };
-	static int map[][];
+	static int map[][][];
 	static Queue<Robot> q;
 
 	public static void main(String[] args) throws IOException {
@@ -19,7 +19,7 @@ public class BOJ8972 {
 
 		R = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
-		map = new int[R][C];
+		map = new int[2][R][C];
 		q = new LinkedList<>();
 
 		for (int r = 0; r < R; r++) {
@@ -32,7 +32,7 @@ public class BOJ8972 {
 					IC = c;
 				} // 이상한 아두이노의 위치
 				else if (now == 'R') {
-					map[r][c] = 1;
+					map[0][r][c] = 1;
 					q.add(new Robot(r, c));
 				}
 			}
@@ -42,15 +42,16 @@ public class BOJ8972 {
 		
 		int res = process(command);
 		// 중간에 게임이 끝나는 경우
-		if (res != -1) {
+		if (res > 0) {
 			System.out.println("kraj " + res);
 		} else {
-			map[IR][IC] = -1;
+			res *= -1;
+			map[res][IR][IC] = -1;
 			for (int r = 0; r < R; r++) {
 				for (int c = 0; c < C; c++) {
-					if(map[r][c] == 0) System.out.print('.');
-					else if(map[r][c] == -1) System.out.print('I');
-					else if(map[r][c] == 1) System.out.print('R');
+					if(map[res][r][c] == 0) System.out.print('.');
+					else if(map[res][r][c] == -1) System.out.print('I');
+					else if(map[res][r][c] == 1) System.out.print('R');
 				}
 				System.out.println();
 			}
@@ -59,22 +60,28 @@ public class BOJ8972 {
 
 	private static int process(String command) {
 		
+		
+		int idx = 0;
 		for (int i = 0; i < command.length(); i++) {
+			// 이전 턴에서의 map 상태를 복사
+			copy(idx);
+			
 			// 1. 종두이노가 이동
 			int com = command.charAt(i) - '0';
 			IR += dr[com];
 			IC += dc[com];
 
 			// 2. 종두이노가 이상한 아두이노가 있는 칸으로 이동한 경우 게임 종료
-			if (map[IR][IC] == 1) return i + 1;
+			if (map[idx][IR][IC] == 1) return i + 1;
 
-			// 3. 이상한 아두이노는 8가지 방향 중에서 종수의 아두이노와 가장 가까워 지는 방향으로 한 칸 이동한다.  
+			// 3. 이상한 아두이노는 8가지 방향 중에서 종수의 아두이노와 가장 가까워지는 방향으로 한 칸 이동한다.  
 			int size = q.size();
 			Robot mLoc = new Robot(0, 0);
 			while (size-- > 0) {
+				
 				Robot now = q.poll();
 				// 아두이노끼리 폭발이 일어난 자리라면 pass
-				if(map[now.r][now.c] == 0) continue;
+				if(map[idx][now.r][now.c] == 0) continue;
 				
 				int mDist = Integer.MAX_VALUE;
 				for (int d = 1; d <= 9; d++) {
@@ -91,9 +98,9 @@ public class BOJ8972 {
 				}
 				// 이동할 좌표
 				q.add(mLoc);
-				map[mLoc.r][mLoc.c]++;
+				map[idx^1][mLoc.r][mLoc.c]++;
 				
-				map[now.r][now.c]--;
+				map[idx^1][now.r][now.c]--;
 				
 				// 4. 이상한 아두이노가 종두이노가 있는 칸으로 이동한 경우에는 게임 종료
 				if (mLoc.r == IR && mLoc.c == IC) return i + 1;
@@ -102,12 +109,24 @@ public class BOJ8972 {
 			// 5. 같은 칸에 있는 이상한 아두이노는 모두 파괴
 			for (int r = 0; r < R; r++) {
 				for (int c = 0; c < C; c++) {
-					if(map[r][c] > 1) map[r][c] = 0;
+					if(map[idx^1][r][c] > 1) map[idx^1][r][c] = 0;
 				}
+			}
+			// 새로운 map을 Main map으로 전환
+			idx ^= 1;
+		}
+		
+		return idx * -1;
+	}
+
+	private static void copy(int idx) {
+		
+		for (int i = 0; i < R; i++) {
+			for (int j = 0; j < C; j++) {
+				map[idx^1][i][j] = map[idx][i][j];
 			}
 		}
 		
-		return -1;
 	}
 
 	static class Robot {
@@ -121,10 +140,3 @@ public class BOJ8972 {
 	}
 
 }
-
-/*
-
-아두이노는 동시에 이동한다는 점을 간과..
-
-
-*/
