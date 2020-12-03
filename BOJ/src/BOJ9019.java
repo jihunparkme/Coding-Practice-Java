5fe1156e4d5f6b82579e11965c0ad5972bfc5b02
박지훈 <pjh073154@gmail.com>
2020-12-03 18:43:15 +0900

    PS 20201203

--- BOJ/src/BOJ9019.java
+++ BOJ/src/BOJ9019.java
@@ -26,48 +26,45 @@ public class BOJ9019 {
 		System.out.println(sb);
 	}
 
+	static String[] oper = {"D", "S", "L", "R"};
 	private static String process(int A, int B) {
 
 		Queue<Info> q = new LinkedList<>();
 		boolean[] checked = new boolean[10000];
 		q.add(new Info(A, ""));
 		
-		int res = 0, tmp = 0, zero = 0;
 		while(!q.isEmpty()) {
 			
 			Info now = q.poll();
 			if(now.n == B) return now.comd;
 			
-			// D 는 n을 두 배로
-			res = (now.n * 2) % 10000;
-			if(!checked[res]) {
-				checked[res] = true;
-				q.add(new Info(res, now.comd + "D"));
-			}
-			
-			// S 는 n에서 1 을 뺀 결과. n이 0 이라면 9999 가 대신 레지스터에 저장된다.
-			res = now.n == 0 ? 9999 : now.n - 1;
-			if(!checked[res]) {
-				checked[res] = true;
-				q.add(new Info(res, now.comd + "S"));
-			}
-
-			// L 은 n의 각 자릿수를 왼편으로 회전(d2, d3, d4, d1)
-			res = (now.n % 1000) * 10 + (now.n / 1000);
-			if(!checked[res]) {
-				checked[res] = true;
-				q.add(new Info(res, now.comd + "L"));
-			}
-			
-			// R 은 n의 각 자릿수를 오른편으로 회전(d4, d1, d2, d3)
-			res = (now.n % 10) * 1000 + (now.n / 10);
-			if(!checked[res]) {
-				checked[res] = true;
-				q.add(new Info(res, now.comd + "R"));
+			for (int i = 0; i < 4; i++) {
+				int res = command(now, i);
+				
+				if(!checked[res]) {
+					checked[res] = true;
+					q.add(new Info(res, now.comd + oper[i]));
+				}
 			}
 		}
 		
 		return "";
+	}
+
+	private static int command(Info now, int comd) {
+		
+		switch(comd) {
+		// D 는 n을 두 배로
+		case 0: return (now.n * 2) % 10000;
+		// S 는 n에서 1 을 뺀 결과. n이 0 이라면 9999 가 대신 레지스터에 저장된다.
+		case 1: return now.n == 0 ? 9999 : now.n - 1;
+		// L 은 n의 각 자릿수를 왼편으로 회전(d2, d3, d4, d1)
+		case 2: return (now.n % 1000) * 10 + (now.n / 1000);
+		// R 은 n의 각 자릿수를 오른편으로 회전(d4, d1, d2, d3)
+		case 3: return (now.n % 10) * 1000 + (now.n / 10);
+		}
+		
+		return 0;
 	}
 
 	static class Info {
