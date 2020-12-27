504dacce523fad32011233509c803813bbc00ab5
박지훈 <pjh073154@gmail.com>
2020-12-27 19:43:51 +0900

    PS 20201227

--- BOJ/src/BOJ3197.java
+++ BOJ/src/BOJ3197.java
@@ -7,11 +7,13 @@ import java.util.StringTokenizer;
 
 public class BOJ3197 {
 
-	static int R, C, cntIce;
+	static int R, C, cur, nxt;
 	static char map[][];
-	static Queue<Point> iceList[];
-	static Point swans[];
+	static boolean[][] visited, vsitSwan;
 	static int[] dr = {-1, 0, 1, 0}, dc = {0, -1, 0, 1};
+	static Queue<Point>[] q;
+	static Queue<Point> qSwan;
+	static Point swans[];
 	
 	public static void main(String[] args) throws IOException {
 		
@@ -20,32 +22,25 @@ public class BOJ3197 {
 		
 		R = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
-		map = new char[R + 2][C + 2];
+		map = new char[R][C];
 		swans = new Point[2];
 		
-		// 사이드 채우기
-		for (int c = 0; c <= C + 1; c++) {
-			map[0][c] = '.';
-			map[R + 1][c] = '.';
-		}
-		for (int r = 0; r <= R + 1; r++) {
-			map[r][0] = '.';
-			map[r][C + 1] = '.';
-		}
+		q = new LinkedList[2];
+		q[0] = new LinkedList<>();
+		q[1] = new LinkedList<>();
+		cur = 0; nxt = 1;
 		
 		int idx = 0;
 		// 두 마리의 백조의 위치
-		for (int i = 1; i <= R; i++) {
-			char[] tmp = br.readLine().toCharArray();
-			for (int j = 1; j <= C; j++) {
-				map[i][j] = tmp[j - 1];
+		for (int i = 0; i < R; i++) {
+			map[i] = br.readLine().toCharArray();
+			for (int j = 0; j < C; j++) {
 				// 백조일 경우
 				if(map[i][j] == 'L') {
 					swans[idx++] = new Point(i, j);
 					map[i][j] = '.';
-				} else if(map[i][j] == 'X') {
-					// 빙판 공간일 경우
-					cntIce++;
+				} else if(map[i][j] == '.') {
+					q[cur].add(new Point(i, j));
 				}
 			}
 		}
@@ -57,46 +52,91 @@ public class BOJ3197 {
 	private static int process() {
 		
 		int time = 0;
-		boolean visited[][] = new boolean[R + 2][C + 2];
-		Queue<Point> q[] = new LinkedList[2];
-		q[0] = new LinkedList<>();
-		q[1] = new LinkedList<>();
-		q[0].add(new Point(0, 0));
+		visited = new boolean[R][C];
 		
-		int cur = 0, nxt = 0;
-		while(cntIce > 0) {
+		while(true) {
 			++time;
 			
-			while(!q[cur].isEmpty()) {
-				Point now = q[cur].poll();
-				// 4방 탐색
-				for (int d = 0; d < 4; d++) {
-					int rr = now.r + dr[d];
-					int cc = now.c + dc[d];
-					// 범위를 초과하거나 이미 방문했을 경우
-					if(rr < 1 || cc < 1 || rr > R || cc > C || visited[rr][cc]) continue;
-					// 빙산일 경우
-					if(map[rr][cc] == 'X') {
-						// 물 공간과 접촉한 모든 빙판 공간이 녹는다.
-						map[rr][cc] = '.';
-						q[nxt].add(new Point(rr, cc));
-						cntIce--;
-					} else { // 물일 경우
-						visited[rr][cc] = true;
-						q[cur].add(new Point(rr, cc));
-					}
-				}
-			}
+			melt();
+//			printMap();
 			
 			// 백조가 만날 수 있는지 확인
-			
-			// 만날 수 있다면 return
+			if(isMeet()) 
+				return time;
 			
 			cur ^= 1;
 			nxt ^= 1;
 		}
+	}
+
+	private static boolean isMeet() {
 		
-		return time;
+		vsitSwan = new boolean[R][C];
+		qSwan = new LinkedList<>();
+		qSwan.add(swans[0]);
+		vsitSwan[swans[0].r][swans[0].c] = true;
+		
+		for (int s = 0; s < 2; s++) {
+			while(!qSwan.isEmpty()) {
+				Point now = qSwan.poll();
+				
+				// 4방 탐색
+				for (int d = 0; d < 4; d++) {
+					int rr = now.r + dr[d];
+					int cc = now.c + dc[d];
+					// 범위를 초과할 경우
+					if(rr < 0 || cc < 0 || rr >= R || cc >= C) continue;
+					// 이미 방문했거나 빙산일 경우
+					if(vsitSwan[rr][cc] || map[rr][cc] == 'X') continue;
+					// 상대 백조를 만났을 경우
+					if(rr == swans[1].r && cc == swans[1].c) return true;
+					
+					vsitSwan[rr][cc] = true;
+					qSwan.add(new Point(rr, cc));
+				}
+			}
+		}
+		
+		return false;
+	}
+
+
+	private static void melt() {
+		
+		while(!q[cur].isEmpty()) {
+			Point now = q[cur].poll();
+			visited[now.r][now.c] = true;
+			
+			// 4방 탐색
+			for (int d = 0; d < 4; d++) {
+				int rr = now.r + dr[d];
+				int cc = now.c + dc[d];
+				// 범위를 초과하거나 이미 방문했을 경우
+				if(rr < 0 || cc < 0 || rr >= R || cc >= C || visited[rr][cc]) continue;
+				
+				visited[rr][cc] = true;
+				
+				// 빙산일 경우
+				if(map[rr][cc] == 'X') {
+					// 물 공간과 접촉한 모든 빙판 공간이 녹는다.
+					map[rr][cc] = '.';
+					q[nxt].add(new Point(rr, cc));
+				} else { // 물일 경우
+					q[cur].add(new Point(rr, cc));
+				}
+			}
+		}
+		
+	}
+	
+	private static void printMap() {
+		
+		for (int i = 0; i < R; i++) {
+			for (int j = 0; j < C; j++) {
+				System.out.print(map[i][j]);
+			}System.out.println();
+		}
+		System.out.println();
 	}
 
 	static class Point {
@@ -107,6 +147,7 @@ public class BOJ3197 {
 			this.c = c;
 		}
 	}
+	
 }
 
 /*
