4b0d493fa39e11b7bcc20468e1329ba2e278c22c
박지훈 <pjh073154@gmail.com>
2020-12-29 18:18:02 +0900

    PS 20201229

--- BOJ/src/BOJ17141.java
+++ BOJ/src/BOJ17141.java
@@ -1,96 +1,120 @@
-import java.awt.Point;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class BOJ17141 {
 	
-	static int N, M, wallCnt = 0, res = 0, map[][];
-	static boolean visited[][];
-	static int[] dx = {-1, 0, 1, 0}, dy = {0, -1, 0, 1};
+	static final int max = Integer.MAX_VALUE;
+	
+	static int N, M, cntSpace, minTime, map[][];
+	static List<Point> virusZone;
+	static Point[] virus;
+	static boolean[][] visited;
 	static Queue<Point> q;
-	static ArrayList<Point> virus;
+	static int[] dr= {-1, 0, 1, 0}, dc = {0, 1, 0, -1};
 	
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
-		st = new StringTokenizer(br.readLine(), " ");
-		N = Integer.parseInt(st.nextToken());	// 세로
-		M = Integer.parseInt(st.nextToken());	// 가로
-		map = new int[N][M];
-		virus = new ArrayList<>();
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());	// 연구소 크기
+		M = Integer.parseInt(st.nextToken());	// 놓을 수 있는 바이러스의 개수
+		map = new int[N][N];
+		virus = new Point[M];
+		virusZone = new ArrayList<>(); // 바이러스를 놓을 수 있는 칸
 		
 		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			for (int j = 0; j < M; j++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
-				// 벽 개수
-				if(map[i][j] == 1) wallCnt++;
-				// 바이러스의 위치 저장
-				if(map[i][j] == 2) virus.add(new Point(i, j));
+				// 빈 공간일 경우
+				if(map[i][j] != 1) cntSpace++;
+				// 바이러스를 놓을 수 있는 공간일 경우
+				if(map[i][j] == 2) virusZone.add(new Point(i, j));
 			}
 		}
 		
-		// 재귀로 3개의 벽을 설치해보자.
-		setupWall(0);
-
-		System.out.println(res);
+		minTime = max;
+		cntSpace -= M; // 바이러스를 놓은 공간은 제외 
+		process(0, 0);
+		
+		// 어떻게 놓아도 모든 빈 칸에 바이러스를 퍼뜨릴 수 없는 경우
+		if(minTime == max) System.out.println(-1);
+		else System.out.println(minTime);
 	}
 	
-	public static void setupWall(int cnt) {
-		// 벽을 세 개 모두 설치했다면
-		if(cnt == 3) {
-			// 바이러스를 퍼뜨려보자.
-			spreadVirus();
+	private static void process(int cnt, int start) {
+		
+		// 모든 바이러스를 설치
+		if(cnt == M) {
+			int time = spreadVirus();
+			minTime = Math.min(minTime, time);
+			
 			return;
 		}
+				
+		for (int i = start; i < virusZone.size(); i++) {
+			// 이 구역에 바이러스를 설치
+			virus[cnt] = virusZone.get(i);
 			
-		for (int i = 0; i < N*M; i++) {
-			// 빈 칸일 경우
-			if(map[i/M][i%M] == 0) {
-				// 벽을 세워볼까
-				map[i/M][i%M] = 1;
-				setupWall(cnt+1);
-				// 벽을 그냥 안 세울래
-				map[i/M][i%M] = 0;
-			}
+			process(cnt + 1, i + 1);
 		}
 	}
-	
-	public static void spreadVirus() {
-		int virusCnt = 0;
-		visited = new boolean[N][M];
+
+	private static int spreadVirus() {
+		
+		int cntSpread = 0, time = -1;
+		visited = new boolean[N][N];
 		q = new LinkedList<>();
-		// 바이러스의 좌표들을 하나씩 확인하면서
-		for (int i = 0; i < virus.size(); i++) {
-			Point v = virus.get(i);
-			// Queue에 넣어주고
-			q.add(new Point(v.x, v.y));
-			// 바이러스를 퍼뜨려보자.
-			while(!q.isEmpty()) {
+		// 초기 설치된 바이러스의 위치
+		for (int i = 0; i < M; i++) {
+			q.add(virus[i]);
+			visited[virus[i].r][virus[i].c] = true;
+		}
+
+		// 바이러스 확산
+		while(!q.isEmpty()) {
+			
+			++time;
+			int size = q.size();
+			
+			while(size-- > 0) {
 				Point now = q.poll();
 				
 				for (int d = 0; d < 4; d++) {
-					int xx = now.x + dx[d];
-					int yy = now.y + dy[d];
+					int rr = now.r + dr[d];
+					int cc = now.c + dc[d];
 					// 범위를 벗어나면 pass
-					if(xx < 0 || yy < 0 || xx >= N || yy >= M) continue;
-					// 빈곳이 아니거나 이미 방문한 곳이면 pass
-					if(map[xx][yy] != 0 || visited[xx][yy]) continue;
+					if(rr < 0 || cc < 0 || rr >= N || cc >= N) continue;
+					// 벽이거나 이미 방문한 곳이면 pass
+					if(map[rr][cc] == 1 || visited[rr][cc]) continue;
 					// 빈 칸이면 바이러스로 감염시키고 Queue에 add
-					visited[xx][yy] = true;
-					virusCnt++;
-					q.add(new Point(xx,yy));
+					visited[rr][cc] = true;
+					cntSpread++;
+					q.add(new Point(rr,cc));
 				}
 			}
 		}
-		// 바이러스가 모두 퍼졌을 때, 안전 영역의 크기를 count
-		res = Math.max(res, (N * M) - wallCnt - 3 - virusCnt - virus.size());
+		
+		// 모든 빈 칸에 바이러스가 퍼졌다면
+		if(cntSpread == cntSpace) return time;
+		return max;
+	}
+
+	static class Point {
+		int r, c;
+
+		public Point(int r, int c) {
+			this.r = r;
+			this.c = c;
+		}
+		
 	}
 }
